94285b18eb42e040e45f6959294239efc1670686
Yan Lagarejo <clagarejo89@gmail.com>
2024-02-14 19:33:32 -0500

    serach iimage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import './Header.css'
+import './Content.css'
+import './Article.css'
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([])
+
+  const open = (url) => {
+    window.open(url)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: '' }}
+          onSubmit={async values => {
+            // Llamado a la api de unsplash
+            const response = await fetch(`https://api.unsplash.com/search/photos?per_page=23&query=${values.search}`, {
+              headers: {
+                'Authorization': 'Client-ID bgm7JnKW3Y0xTsvJaoNgMOeXbi5W2HBnN0VMawa4CEY'
+              }
+            })
+
+            const data = await response.json()
+            setPhotos(data.results)
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" placeholder="Buscador de imagenes"/>
+          </Form>
+        </Formik>
       </header>
-    </div>
+      <div className="container">
+        <div className="center">
+          {photos.map(photo => 
+            <article key={photo.id} onClick={() => open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.description}/>
+              <p>{[photo.description, photo.alt_description].join(' - ')}</p>
+            </article>
+          )}
+        </div>
+      </div>
+     </div>
   );
 }
 
-export default App;
+export default App
